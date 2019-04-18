0fba7fc35243e33341869c34cdae95f7b076e940
Nick Debeurre <ndebeurr@asu.edu>
2019-04-18 12:17:20 -0700

    added support for length piping

---
+++ progress_bar_command_line.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+void progress_bar(int loop, char bar[])
+{
+    fflush(stdout);
+    bar[loop] = '|';
+    printf("\r0%% |%s| 100%%", bar);
+}
+
+void init(char bar[], int length)
+{
+    int i;
+    for (i = 0; i < length; i++)
+        bar[i] = '-';
+}
+
+void wait()
+{
+    int i = 0;
+    while (i < 50000000)
+        i++;
+}
+
+int main(void)
+{
+    int bar_length = 50;
+    while (1)
+    {
+        char bar[bar_length];
+        init(bar, bar_length);
+        int i;
+        for (i = 0; i < bar_length; i++)
+        {
+            progress_bar(i, bar);
+            wait();
+        }
+        printf("\n");
+    }
+    return 0;
+}
