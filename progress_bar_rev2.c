2192d83268bc0fc77b43c6f7ac8097845ccab564
Nick Debeurre <ndebeurr@asu.edu>
2019-04-18 11:47:20 -0700

    revision 3

--- progress_bar_rev2.c
+++ progress_bar_rev2.c
@@ -7,6 +7,13 @@ void progress_bar(int loop, char bar[])
     printf("\r0%% |%s| 100%%", bar);
 }
 
+void init(char bar[], int length)
+{
+    int i;
+    for (i = 0; i < length; i++)
+        bar[i] = '-';
+}
+
 void wait()
 {
     int i = 0;
@@ -16,14 +23,18 @@ void wait()
 
 int main(void)
 {
-    char bar[30] = "------------------------------";
-    int i;
-    int loop = 30;
-    for (i = 0; i < loop; i++)
+    int bar_length = 50;
+    while (1)
     {
-        progress_bar(i, bar);
-        wait();
+        char bar[bar_length];
+        init(bar, bar_length);
+        int i;
+        for (i = 0; i < bar_length; i++)
+        {
+            progress_bar(i, bar);
+            wait();
+        }
+        printf("\n");
     }
-    printf("\n");
-return 0;
+    return 0;
 }
